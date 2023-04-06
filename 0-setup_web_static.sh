cd0de66735aa8de60d3b8d93d2477828c997e68f
Khaikhai01 <shishchei@gmail.com>
2023-04-06 01:39:44 -0700

    html dummy file updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,15 @@ sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 
 #dummy html file
-sudo tee /data/web_static/releases/test/index.html <<< "Holberton School"
+#dummy html file
+dummy_html='''<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>'''
+sudo tee /data/web_static/current/index.html <<< "$dummy_html"
 
 # creating symbolic link between the files
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
