ad48dab446d2be733be968df4de841354d26e75f
Khaikhai01 <shishchei@gmail.com>
2023-04-07 01:52:14 -0700

    deploying issue fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,9 @@ from fabric.api import env, put, run
 import os.path
  
 env.hosts = ["100.25.204.11", "52.3.245.182"]
- 
+env.private_key = '~/.ssh/school'
+env.usr_name = 'ubuntu'
+
 def do_deploy(archive_path):
     ''' checking is file path exists'''
     if not os.path.isfile(archive_path):
@@ -18,13 +20,15 @@ def do_deploy(archive_path):
 
         put(archive_path, "/tmp/")
 
-        run("mkdir - p {}".format(remote_folder))
+        run("sudo mkdir -p {}".format(remote_folder))
 
-        run("tar -xvzf /tmp/{} -C {}".format(compressed, remote_folder))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed, remote_folder))
 
-        run("rm /tmp/{}".format(compressed))
-
-        run("ln -sf {} {}".format(remote_folder, symbolic_conn))
+        run("sudo rm /tmp/{}".format(compressed))
+        run('sudo mv {}/web_static/* {}'.format(remote_folder, remote_folder))
+        run('sudo rm -rf {}/web_static'.format(remote_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -sf {} {}".format(remote_folder, symbolic_conn))
         return True
     except Exception as e:
         return False
