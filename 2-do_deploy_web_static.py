b277610af936e40ee873791b6aeb4d8fdf9a94a4
Khaikhai01 <shishchei@gmail.com>
2023-04-09 15:34:06 -0700

    shellcheck

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3i
+#!/usr/bin/python3
 """
 Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers, using the function do_deploy
@@ -7,8 +7,8 @@ from fabric.api import env, put, run
 import os.path
 
 env.hosts = ["100.25.204.11", "52.3.245.182"]
-env.private_key = '~/.ssh/school'
-env.usr_name = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
