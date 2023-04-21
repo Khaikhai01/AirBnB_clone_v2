b601ba1cb7a05ecca82a576015ea2e9677d003c2
purity <shishchei@gmail.com>
2023-04-21 01:24:04 -0700

    work on if statement

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,9 +47,11 @@ def template_index(n):
 def odd_or_even(n):
     '''checks if n is even or odd'''
     if n % 2 == 0:
-        odd_or_even = 'even'
-    odd_or_even = 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, num=odd_or_even)
+        odd_or_even = "even"
+    else:
+        odd_or_even = "odd"
+
+    return render_template("6-number_odd_or_even.html", n=n, num=odd_or_even)
 
 
 if __name__ == "__main__":
