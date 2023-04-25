f8b3e38a88f0bc85bb5dc241125fa7c577ac371d
Purity <shishchei@gmail.com>
2023-04-25 08:56:18 +0000

    changes made

--- models/base_model.py
+++ models/base_model.py
@@ -41,7 +41,7 @@ class BaseModel:
         """Returns a string representation of the instance"""
         dictionary = self.__dict__.copy()
         dictionary.pop("_sa_instance_state", None)
-        return f'[{self.__class__.__name__}] ({self.id}) {dictionary}'
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
